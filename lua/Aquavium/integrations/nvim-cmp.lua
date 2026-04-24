dcd27be94f3cd784abf132e712f2cfe2fe85f673
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-25 01:56:18 +0900

    fix: Link CmpBorder highlight to FloatBorder

--- lua/Aquavium/integrations/nvim-cmp.lua
+++ lua/Aquavium/integrations/nvim-cmp.lua
@@ -4,7 +4,7 @@ local M = {}
 function M.apply(c, opts)
     local hl = {
         CmpNormal = { fg = c.fg, bg = c.bg1 },
-        CmpBorder = { fg = c.gray, bg = c.bg1 },
+        CmpBorder = { link = "FloatBorder" },
         CmpMenuSel = { fg = vim.api.nvim_get_hl(0, { name = "Special" }).fg, bold = opts.bold },
     }
 
